a4c8ab4fc425cdf8b551102db539e52e7faf62e8
Carl Helmertz <helmertz@gmail.com>
2010-11-25 22:41:21 +0100

    Add: examples and logic in readme, another demo

--- jquery.fineprint.js
+++ jquery.fineprint.js
@@ -1,70 +1,116 @@
 jQuery.fn.fineprint = function(options) {
 
-	var submitButton = this;
-
-	if("object" !== typeof options || "undefined" == typeof options.text) {
+	if("object" !== typeof options || "undefined" == typeof options.submit) {
 		return this;
 	}
 
 	// todo: make compatible with jquery-ui's .highlight()
 	var settings = {
+		afterCountdown: function() {},
+		beforeCountdown: function() {},
 		countdown: 30,
 		dataKey: 'fineprintWeight',
+		initialDelay: 5,
 		style: {
 			'backgroundColor': 'black',
 			'color': '#fff',
 			'fontSize': '30px',
-			'fontWeight': 'bold'
-		}
+			'fontWeight': 'bold',
+			'padding': '3px'
+		},
+		temporaryButtonValue: 'Read the highlighted text before accepting'
 	};
 
 	$.extend(settings, options);
 
-	// Make the user wait a bit to actually notice the fineprint
+	var submitButton = options.submit;
 	var originalButtonText = submitButton.val();
-	submitButton.attr('disabled', 'disabled').val('Read the license!');
-
-	var oldStyle = {};
-
-	var text = options.text;
-	var emphasizingElements = text.find('strong, em');
-	var totalCharactercount = emphasizingElements.text().length;
-
-	// Loop-de-loop, iterate over each element and apply the highlight
-	var totalReservedTime = 0;
-	$.each(emphasizingElements, function(index, value) {
-		var current = $(value);
-
-		setTimeout(
-			function() {
-				// Save the original CSS, for a return point
-				for(var key in settings.style) {
-					oldStyle[key] = current.css(key);
-					
-					// Apply the new CSS rule
-					current.css(key, settings.style[key]);
-				}
-			},
-			totalReservedTime
-		);
-
-		// The currently emphasized text only last for a calculated amount of time
-		var miliseconds = settings.countdown * (current.text().length/totalCharactercount) * 1000;
-		totalReservedTime += parseInt(miliseconds);
-		setTimeout(
-			function() {
-				// Doing the reset dance
-				for(var key in settings.style) {
-					current.css(key, oldStyle[key]);
-				}
-			},
-			totalReservedTime
-		);
-
-	});
+	submitButton.attr('disabled', 'disabled');
 	
 	// Enable the form again
-	submitButton.attr('disabled', '').val(originalButtonText);
+	var done = function() {
+		// todo make sure the button is in the viewport (even a bit in).. I think it outweighs the annoyance in scrolling the license away, since the plugin is all about skimming the text anyway
+		submitButton.attr('disabled', '').val(originalButtonText);
+		if(typeof settings.afterCountdown === "function") {
+			settings.afterCountdown();
+		}
+	};
+
+	if(settings.temporaryButtonValue.indexOf('%s') != -1) {
+		// The configuration included the magical '%s' which tell us a countdown should be started
+		var secondsLeft = settings.countdown+settings.initialDelay;
+		console.log(settings);
+		var interval = setInterval(
+			function() {
+				if(1 === secondsLeft) {
+					done();
+					clearInterval(interval);
+					return;
+				}
+				submitButton.val(settings.temporaryButtonValue.replace('%s', secondsLeft));
+				secondsLeft -= 1;
+			},
+			1000	
+		);
+	} else {
+		// The button keeps a temporary value throughout the entire countdown... a bit boring
+		submitButton.val(settings.temporaryButtonValue);
+	}
+
+	// Container used on a per-element basis, containing the pre-enhanced stylesheet rules of the strong
+	var oldStyle = {};
+
+	var text = this;
+	var emphasizingElements = text.find('strong, em');
+	var emphasizingElementsCount = emphasizingElements.length;
+	var totalCharactercount = emphasizingElements.text().length;
+
+	var totalReservedTime = 0;
+
+	// Let the user scan the page before throwing emphasized wordings at her
+	setTimeout(
+		function() {
+			if(typeof settings.beforeCountdown === "function") {
+				settings.beforeCountdown();
+			}
+			$.each(emphasizingElements, function(index, value) {
+				var current = $(value);
+
+				setTimeout(
+					function() {
+						// Save the original CSS, for a return point
+						for(var key in settings.style) {
+							oldStyle[key] = current.css(key);
+							
+							// Apply the new CSS rule
+							current.css(key, settings.style[key]);
+						}
+					},
+					totalReservedTime
+				);
+
+				// The currently emphasized text only last for a calculated amount of time
+				var miliseconds = settings.countdown * (current.text().length/totalCharactercount) * 1000;
+				totalReservedTime += parseInt(miliseconds);
+				setTimeout(
+					function() {
+						// Doing the reset dance
+						for(var key in settings.style) {
+							current.css(key, oldStyle[key]);
+						}
+
+						// If the iteration is over, the user should be able to continue by submitting the form
+						if(index === emphasizingElementsCount - 1) {
+							done();
+						}
+					},
+					totalReservedTime
+				);
+
+			});
+		},
+		parseInt(settings.initialDelay)*1000
+	);
 	
 	// Enable chaining
 	return this;
--- readme.md
+++ readme.md
@@ -1,2 +1,30 @@
 # fineprint
 Add usability to your license agreement or otherwise really, really important stuff.
+
+## Usage
+$('#license_agreement').fineprint({
+	'countdown': 20, // How long should the fineprint-enhancement last?
+	'initialDelay': 5, // how long should the 
+	'submit': $('#submit'),
+});
+
+Scenario: the #submit-element will be deactivated on page load, every **strong/em** element that is a child element of #license_agreement will be highlighted in order, during 20 seconds (countdown), starting 5 seconds after page load (initialDelay).
+
+The amount of time an element is highlighted depends on its length compared to the sum of all texts-to-be-highlighted in #license_agreement.
+
+### Example 1
+	
+	<div id="licence_agreement">
+		<strong>This is very important</strong>, this not so much, but
+		<strong>this is also important</strong>.
+	</div>
+
+The html above will enhance *This is very important* for 10 seconds, thereafter *this is also important* for 10 seconds. The strings are of equal length, therefore they get equal amount of time in the spotlight.
+
+### Example 2
+
+	<div id="license_agreement">
+		<strong>Hello there!</strong>, isn't this just <strong>that plugin</strong> you've <strong>been looking for?</strong>
+	</div>
+
+ 11 vs 10 vs 16 letters means that the first text will be displayed in 11 / 37 * 20 seconds.
